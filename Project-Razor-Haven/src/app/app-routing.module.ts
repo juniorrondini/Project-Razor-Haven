03d91976f174c113703d5cfda69529a1d91ba644
juniorrondini <jjrondinijj@gmail.com>
2023-07-14 17:15:19 -0300

    alterações gerais

--- Project-Razor-Haven/src/app/app-routing.module.ts
+++ Project-Razor-Haven/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AgendamentosComponent } from './components/agendamentos/agendamentos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // outras rotas
+  { path: 'agendamentos', component: AgendamentosComponent },
+  // outras rotas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
